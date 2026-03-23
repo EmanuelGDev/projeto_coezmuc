60093a4deefda3f7fb5b6a53415a8698e68ee596
Emanuel <emanuel.gui.santana@gmail.com>
2026-03-23 14:46:14 -0300

    [ADD]feat(user): add new attribute to user model

--- src/lib/scheema.ts
+++ src/lib/scheema.ts
@@ -4,6 +4,7 @@ const User = new mongoose.Schema({
     username: { type: String, required: true,},
     password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    isAdmin : {type : Boolean},
     createdAt: { type: Date, default: Date.now }
 });
 
