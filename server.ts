fad79cf122d3893f4962d5dc91333fcf0ba94a68
Emanuel <emanuel.gui.santana@gmail.com>
2026-05-09 21:15:16 -0300

    test

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
-import "dotenv/config";
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env.local' });
 import fastify from 'fastify';
 import cors from '@fastify/cors'
 import mongoose from 'mongoose';
